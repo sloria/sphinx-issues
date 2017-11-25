9ef38e4a8621718cb9fe60887928567ac9d133d1
Steven Loria <sloria1@gmail.com>
2017-11-25 09:28:15 -0500

    Add PyPI deployment

--- tasks.py
+++ tasks.py
@@ -31,14 +31,3 @@ def clean(ctx):
     ctx.run('rm -rf dist')
     ctx.run('rm -rf sphinx-issues.egg-info')
     print('Cleaned up.')
-
-@task
-def publish(ctx, test=False):
-    """Publish to the cheeseshop."""
-    clean(ctx)
-    if test:
-        ctx.run('python setup.py register -r test sdist bdist_wheel', echo=True)
-        ctx.run('twine upload dist/* -r test', echo=True)
-    else:
-        ctx.run('python setup.py register sdist bdist_wheel', echo=True)
-        ctx.run('twine upload dist/*', echo=True)
